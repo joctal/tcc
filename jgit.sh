6f844878e405182952f3a926879a30f0753a9f27
root <root@tcc-tools.localdomain>
2020-03-01 16:05:04 -0300

    jss_servidor_tangram

---
+++ jgit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git config --global core.preloadindex true
+git config --global core.fscache true
+git config --global gc.auto 256
+if [ `git status | grep -e "modified" -e "new file" | wc -l` -gt 0 ];then
+	git pull
+	git status
+	git add *
+	git add .gitignore
+	git add robot_copia_codigos_FULL.sh
+
+	#git commit -a
+	git commit -m "jss_servidor_tangram $1"
+	git push -u origin master
+	git status
+fi
+
+
